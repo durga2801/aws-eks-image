209ba5c438a14c4f3fb2e8375a3f07c93a58a2e6
durga2801 <durgendra.singh2801@gmail.com>
2022-08-03 08:37:41 +0530

    initial commit

---
+++ src/test/java/com/durgendra/AwsEksImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgendra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEksImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
